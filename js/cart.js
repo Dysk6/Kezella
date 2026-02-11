4136b67c84dc7293690c21503dbfb5d873d07773
Dysk6 <71672148+Dysk6@users.noreply.github.com>
2026-02-11 21:29:37 +0100

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,19 @@
+function addToCart() {
+  if (!selectedSize || !selectedColor) {
+    alert("Please select both a Size and a Color");
+    return;
+  }
+
+  const cartItem = {
+    id: currentProductId,
+    name: product.name,
+    image: product.media[1].src, // Uses the first image
+    price: product.sizes[selectedSize], // Grabs price based on size
+    size: selectedSize,
+    color: selectedColor, // <--- NEW
+    quantity: 1
+  };
+
 function updateCartCount() {
   const cart = JSON.parse(localStorage.getItem("cart")) || [];
   let total = 0;
@@ -13,3 +29,4 @@ function updateCartCount() {
 }
 
 updateCartCount();
+}
